7b0fac988752306bef84ba7a8253c592514b83b5
Christopher Kim <chris.kim1990@gmail.com>
2021-02-08 21:28:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(numbers)
   new_numbers = [9, 10, 16, 25]
   count = 0
   while count < numbers.length do
-    numbers.to_i ** 2
-    new_numbers.to_i ** 2
+    numbers ** 2
+    new_numbers ** 2
     count += 1
   end
 end
