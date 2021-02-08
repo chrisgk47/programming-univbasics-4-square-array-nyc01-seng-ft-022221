603203c0d4a4cb4ae562f4358470c8b030d87df4
Christopher Kim <chris.kim1990@gmail.com>
2021-02-08 21:11:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array, limit)
   count = 0
   while count < limit do
-    array[count] ** 2
+    array(limit) = array[count] ** 2
     count += 1
   end
-  array
+  array(limit)
 end
